a31a09a745505b17701198ed537366c327b28197
wizzybofficial <97391007+wizzybofficial@users.noreply.github.com>
2022-03-19 13:27:40 +0100

    Update and rename game.py to gamer.py

--- gamer.py
+++ gamer.py
@@ -3,6 +3,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("game").gamemain()
+       __import__("gamer").gamemain()
    except Exception as a:
        exit(str(a))
